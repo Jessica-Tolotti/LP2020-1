f34a58b4b6525ba6052189d0839b763880b92fa5
JessicaTolotti <79671249+Jessica-Tolotti@users.noreply.github.com>
2021-03-07 07:45:05 +0000

    Exercício 03 pronto

--- SEMANA01_q02.c
+++ SEMANA01_q02.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
  int main(void){
   int a, b;
@@ -6,3 +7,4 @@
     printf("%i %i %i %i %i %i",a-1, a, a+1, b-1, b, b+1);
  return 0;
 }
+*/
---
+++ SEMANA02_q03.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ int main(void){
+   int a, b, c;
+   scanf("%i %i %i",&a, &b, &c);
+    if((a<c && b<c && a+b<=c)||(c<b && a<b && c+a<=b)|| (c<a && b<a && c+b<=a)){
+       printf("Nao forma\n");
+     }else{
+       if((a==b && b==c && c==a)|| (b==c && c==a && a==b)|| (c==a && a==b && b==c)){
+         printf("Equilatero\n");
+       }else{
+         if((a==b && b==a && a!=c)||( b==c && c==b && b!=a)||(c==a && a==c && c!=b)){
+           printf("Isosceles\n");
+         }else{
+           if((a!=b && a!=c) && (b!=a && b!=c) && (c!=a && c!=b)){
+             printf("Escaleno\n");
+           }
+         }
+       }
+     }
+  return 0;
+ }
