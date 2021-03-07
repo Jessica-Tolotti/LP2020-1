b8b651640c47b38bb8d41337d0ce5414a818b99f
JessicaTolotti <79671249+Jessica-Tolotti@users.noreply.github.com>
2021-03-07 06:42:10 +0000

    Exercício 01 pronto

---
+++ SEMANA01_q01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  float compra, aux;
+    scanf("%f",&compra);
+      if(compra <= 100){
+        aux= compra - ((compra / 100)*0);
+        printf("%.2f", aux);
+      }else{
+        if(compra > 100 && compra <= 200){
+          aux= compra - ((compra / 100)*5);
+         printf("%.2f", aux);
+       }else{
+         aux= compra - ((compra / 100)*10);
+          printf("%.2f", aux);
+       }
+      }
+ return 0;
+}
