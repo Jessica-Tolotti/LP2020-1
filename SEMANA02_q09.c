765416708f873d05eae383dbafe0c4754a5d8326
Jéssica Almeida Tolotti <>
2021-04-09 01:28:55 +0000

    Exercício 09 pronto

---
+++ SEMANA02_q09.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main() {
+
+int Valor, Troco, Cem, Cinquenta, Vinte, Dez, Cinco, Dois, Um;
+	scanf("%i",&Valor);
+  Troco = Valor;
+  Cem = Troco / 100;
+  Troco = Troco - (Cem *100);
+  Cinquenta = Troco / 50;
+  Troco = Troco - (Cinquenta * 50);
+  Vinte = Troco / 20;
+  Troco = Troco - (Vinte * 20);
+  Dez = Troco /10;
+  Troco = Troco -(Dez * 10);
+  Cinco = Troco / 5;
+  Troco = Troco - (Cinco * 5);
+  Dois = Troco / 2;
+  Troco = Troco - (Dois * 2);
+  if(Cem >=1){
+  	printf("%i nota(s) de R$ 100,00\n",Cem);
+  }
+  	if(Cinquenta >=1){
+  	printf("%i nota(s) de R$ 50,00\n",Cinquenta);
+  }
+  	if(Vinte >=1){
+  		printf("%i nota(s) de R$ 20,00\n",Vinte);
+	  }
+	  	if(Dez >=1){
+	  	    printf("%i nota(s) de R$ 10,00\n",Dez);	
+		  }
+		  	if(Cinco >=1){
+		  		printf("%i nota(s) de R$ 5,00\n",Cinco);
+			  }
+			  	if(Dois >=1){
+			  		printf("%i nota(s) de R$ 2,00\n",Dois);
+
+				  }
+				  	if(Troco >=1){
+				  		printf("%i nota(s) de R$ 1,00\n",Troco);
+
+					  }
+				  }
