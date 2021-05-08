8e1afbcc94eb68ba459d89bb842da06f69a178d7
Jéssica Almeida Tolotti <>
2021-05-08 21:10:42 +0000

    exercício 12 pronto

--- SEMANA02_q10.c
+++ SEMANA02_q10.c
@@ -1,32 +1,113 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int main(){
-int n,un,cen,dez,mil,aux;
+int n,aux;
+char mil[9]{'mil', 'dois mil', 'tres mil', 'quatro mil', 'cinco mil', 'seis mil', 'sete mil', 'oito mil', 'nove mil'};
+
   scanf("%i",&n);
-mil = n/1000;
-dez = ((n/100) *10);
-cen = (n/10) *1;
-un = n%1;
+aux = n/1000;
 switch(mil){
-    case 1:  printf("Mil");break;
-    case 2:  printf("dois mil");break;
-    case 3:  printf("tres mil");break;
-    case 4:  printf("quatro mil");break;
-    case 5:  printf("cinco mil");break;
-    case 6:  printf("seis mil");break;
-    case 7:  printf("sete mil");break;
-    case 8:  printf("oito mil");break;
-    case 9:  printf("nove mil");break;
+    case 1:  printf("Mil");
+      break;
+    case 2:  printf("dois mil");
+      break;
+    case 3:  printf("tres mil");
+      break;
+    case 4:  printf("quatro mil");
+      break;
+    case 5:  printf("cinco mil");
+      break;
+    case 6:  printf("seis mil");
+      break;
+    case 7:  printf("sete mil");
+      break;
+    case 8:  printf("oito mil");
+      break;
+    case 9:  printf("nove mil");
+      break;
+     
+
+switch(cen){
+    case 1:  printf("cem");
+      break;
+    case 2:  printf("duzentos");
+      break;
+    case 3:  printf("trezentos");
+      break;
+    case 4:  printf("quatrocentos");
+      break;
+    case 5:  printf("quinhentos");
+      break;
+    case 6:  printf("seicentos");
+      break;
+    case 7:  printf("setecentos");
+      break;
+    case 8:  printf("oitocentos");
+      break;
+    case 9:  printf("novecentos");
+      break;
+
+switch(dez){
+    case 2:  printf("vinte");
+      break;
+    case 3:  printf("trinta");
+      break;
+    case 4:  printf("quarenta");
+      break;
+    case 5:  printf("cinquenta");
+      break;
+    case 6:  printf("sesenta");
+      break;
+    case 7:  printf("setenta");
+      break;
+    case 8:  printf("oitenta");
+      break;
+    case 9:  printf("noventa");
+      break;
+
+    switch(1){
+      case 1: printf("dez");
+        break;
+     case 2:  printf("doze");
+      break;
+    case 3:  printf("treze");
+      break;
+    case 4:  printf("catorze");
+      break;
+    case 5:  printf("quinze");
+      break;
+    case 6:  printf("dezesseis");
+      break;
+    case 7:  printf("dezessete");
+      break;
+    case 8:  printf("dezoito");
+      break;
+    case 9:  printf("dezenove");
+      break;
+      }
+
+switch(un){
+   case 1:  printf("um");
+      break;
+    case 2:  printf("dois");
+      break;
+    case 3:  printf("tres");
+      break;
+    case 4:  printf("quatro");
+      break;
+    case 5:  printf("cinco");
+      break;
+    case 6:  printf("seis");
+      break;
+    case 7:  printf("sete");
+      break;
+    case 8:  printf("oito");
+      break;
+    case 9:  printf("nove");
+      break;
      
 }
-switch(dez){
-    case 1:  printf("cem");break;
-    case 2:  printf("duzentos");break;
-    case 3:  printf("trezentos");break;
-    case 4:  printf("quatrocentos");break;
-    case 5:  printf("quinhentos");break;
-    case 6:  printf("seicentos");break;
-    case 7:  printf("setecentos");break;
-    case 8:  printf("oitocentos");break;
-    case 9:  printf("novecentos");break;
 }
 }
+}
+
+}*/
---
+++ SEMANA07_q12.c
@@ -0,0 +1,16 @@
+/*#include <stdio.h>
+int main(){
+int num,i;
+scanf("%i",&num);
+for(i=1;i<=num;i++){
+   if(i%4 !=0){
+         printf("%i",i);
+   }else{
+     printf("PIN");
+   }
+     
+   
+ 
+}
+  return 0;
+}*/
