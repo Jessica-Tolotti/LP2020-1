feb39a631d630a058ebd4caabf98e7ffdd8c9a59
Jéssica Almeida Tolotti <>
2021-05-08 21:42:22 +0000

    tentei resolver a questao 13

---
+++ SEMANA07_q13.c
@@ -0,0 +1,27 @@
+/*#include <stdio.h>
+int main (){
+ float num,um,dois,tres;
+ int i,n;
+do{ scanf ("%d",&n);
+	 for (i=1;i<=n;i++){
+	 scanf ("%f",&num);
+		 if (num >= um){
+		 dois=um;
+		 tres=dois;
+		 um=num;
+		 }
+			 else{
+			 if (num>=dois){
+				 tres=dois;
+					 dois=num;
+			 }
+					 else if (num >= tres)
+					 tres=num;
+			 }
+	 }
+   
+}while(num != 0);
+	 printf ("%.f %.f %.f\n",um,dois,tres);
+return 0;
+}
+*/
